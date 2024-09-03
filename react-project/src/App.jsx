ee426b8a7d27edd0440f78f5670c3848b1059868
Nishreyta-Lamsal <nishreytal@gmail.com>
2024-09-03 20:12:41 +0545

    Changed some margins

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
   <div className="bg-black px-4 py-8">
   <div className="mb-4">
-    <h1 className=" flex justify-center my-5 text-slate-200 text-3xl font-bold">Blog Components</h1>
+    <h1 className=" flex justify-center mt-5 mb-9 text-slate-200 text-3xl font-bold">Blog Components</h1>
     <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {data.map((item, index) => (
         <div key={index} className="w-full">
