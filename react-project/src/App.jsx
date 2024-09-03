e45374394a9bf174e030e38bdd30673781b6efcb
Nishreyta-Lamsal <nishreytal@gmail.com>
2024-09-03 20:16:08 +0545

    .

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -20,9 +20,6 @@ const App = () => {
     </div>
   </div>
 </div>
-
-
   )
 }
-
 export default App
--- react-project/src/Components/BlogCard.jsx
+++ react-project/src/Components/BlogCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BlogCard = ({image,title,description}) => {
   return (
     <div className="h-full flex flex-col justify-between p-6 rounded cursor-pointer hover:bg-[#ffffff26] transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 shadow-lg">
-        <img className="rounded w-full h-48 object-cover mb-4" src={image} alt={title} />
+        <img className="rounded w-full h-48 object-cover mb-4" src={image} alt="image"/>
             <div className="flex-1">
                <p className="text-slate-200 font-bold text-xl mb-2">{title}</p>
                   <p className="text-slate-200 text-sm mb-4">{description}</p>
